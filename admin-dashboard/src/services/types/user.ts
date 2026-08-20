592e735d890fdaea23309f36d0642a260d6ae91e
sosina <sosinagirmay15@gmail.com>
2026-08-20 23:32:09 +0300

    add a user update feature

--- admin-dashboard/src/services/types/user.ts
+++ admin-dashboard/src/services/types/user.ts
@@ -9,6 +9,18 @@ export interface CreateUserPayload {
   condoCode: string
 }
 
+export interface UpdateUserPayload {
+  fullName?: string
+  email?: string
+  phoneNumber?: string
+  fan?: string
+  condoId?: string
+  block?: string
+  roomNo?: string
+  dueDate?: string
+  isVerified?: boolean
+}
+
 export interface GetUsersResponse {
   success: boolean
   count?: number
